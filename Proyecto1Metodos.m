11f05ee87379d3dd7f873545aa377369c1b6c5bf
Daniel Pineda <daniel-p96@outlook.com>
2021-09-07 16:30:38 -0600

    Usar los datos medidos en el algoritmo para obtener picos.

--- Proyecto1Metodos.m
+++ Proyecto1Metodos.m
@@ -1,45 +1,63 @@
 %% Universidad del Valle de Guatemala
 %  Depto de Matematica
-%  Métodos Numéricos 1, sección 30
+%  Metodos Numericos 1, seccion 30
 %  Proyecto 1
 %  Santiago Fernandez 18171
 %  Camila Lemus 18272
 %  Diego Mencos 18300
 %  Larry Paul 18117
-%  Daniel Barrientos 18***
+%  Daniel Barrientos 18714
 
 close all
 %% Extrayendo Datos del Excel
-A = 4;
-k = -0.5;
-x = linspace(0,10,1000);
-f = 5.5;
-y = A*exp(k*x).*sin(2*pi*f*x);
+test = 0;
+if (test)
+    A = 4;
+    k = -0.5;
+    x = linspace(0,10,1000);
+    f = 5.5;
+    y = A*exp(k*x).*sin(2*pi*f*x);
+end
+
+%% Leyendo del csv
+
+[t, data1, data2] = filter_data('./data.csv', 102, 5/23, 130, 255);
+
+data1 = data1 -5;
+data2 = data2 -5;
+
+x = t';
+y = data2';
 
 %% Separando datos maximos
-y_ant = 0
+y_ant = 0;
 Ymax = [];
 Xmax = [];
 C = 1;
-for n=1:size(x,2)
+for n=1:(size(x,2)-1)
     switch C
         case 1
-            if y_ant>y(n)
+            if y_ant>y(n+1)
                 Ymax = [Ymax y_ant];
-                Xmax = [Xmax x(n-1)];
+                Xmax = [Xmax x(n)];
                 C = 2;
             end
         case 2
-            if y_ant<y(n)
+            if y_ant<y(n+1)
                 Ymax = [Ymax y_ant];
-                Xmax = [Xmax x(n-1)];
+                Xmax = [Xmax x(n)];
                 C = 1;
             end
     end
-    y_ant = y(n);
+    y_ant = y(n+1);
 end
 
-scatter(Xmax,Ymax,1,'r')
+Ymax(1) = abs(y(1)); % quick fix
+
+
+plot(Xmax,abs(Ymax),'-x')
+hold on
+plot(t, abs(data2));
 hold off
 
 %% Obteniendo frecuencia
@@ -65,5 +83,20 @@ plot(x,y,'r')
 hold on
 plot(x,y_reg,'b')
 hold off
-%% Tomando puntos máximos
+%% Tomando puntos maximos
 
+
+%% Funciones
+
+function [t, data1, data2] = filter_data(file, offset, scale, start_x, end_x)
+    data_temp = readtable(file);
+    
+    t = 0:0.004:(size(data_temp.CH1, 1)-1)*0.004;
+    t = t';
+    
+    t = t(1:(end_x - start_x + 1));
+    
+    data1 = (data_temp.CH1(start_x:end_x) - offset)*scale;
+    data2 = (data_temp.CH2(start_x:end_x) - offset)*scale;
+
+end
