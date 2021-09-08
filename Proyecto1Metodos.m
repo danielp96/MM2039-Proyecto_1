1e6c843790abf13071cad9a763f20ab353fa7148
fer18171 <66540300+fer18171@users.noreply.github.com>
2021-09-08 14:57:42 -0600

    Se agregaron comentarios
    Version Final

--- Proyecto1Metodos.m
+++ Proyecto1Metodos.m
@@ -9,8 +9,9 @@
 %  Daniel Barrientos 18714
 
 close all
-%% Extrayendo Datos del CSV
-test = 0;
+%% Datos de Prueba
+
+test = 0; %Datos de prueba que cumplen con el modelo 
 if (test)
     A = 4;
     k = -0.5;
@@ -21,18 +22,19 @@ end
 
 %% Leyendo del csv
 
-[t, data1, data2] = filter_data('./data.csv', 102, 5/23, 130, 255);
+[t, data1, data2] = filter_data('./data.csv', 102, 5/23, 130, 255); %Lee los datos y los ingresa en arrays
 
-data1 = data1 -5;
+data1 = data1 -5; %Centrar los datos en 0
 data2 = data2 -5;
 
-x = t';
+x = t'; %pasar los datos a 'x' y 'y'
 y = data2';
 
-x = x(1:55);
+x = x(1:55); % Recortar los arrays para quitar los datos inservibles
 y = y(1:55);
 
 %% Separando datos maximos
+% Inicializamos 
 y_ant = 0;
 Ymax = [];
 Xmax = [];
@@ -57,10 +59,10 @@ end
 
 Ymax(1) = abs(y(1)); % quick fix
 
-
-plot(Xmax,abs(Ymax),'-x')
+figure(1)
+scatter(x,y,20,'b','filled');
 hold on
-plot(t, abs(data2));
+scatter(Xmax,Ymax,20,'r','filled')
 hold off
 
 %% Obteniendo frecuencia
@@ -70,49 +72,52 @@ f_reg = 1/T_reg;
 %f_reg = 31;
 
 %% Linealizando el exponencial
-Ymax = abs(Ymax);
+Ymax = abs(Ymax); %Pasamos todos los picos negativos a la parte de arriba
 
 Ymax(Ymax==0) = 0.2174; %fixes log(0) issue
 
-Ylineal = log(Ymax);
+Ylineal = log(Ymax); % Hallamos y* como ln(yi)
+
+%Se plantean las matrices
 MA = [size(Xmax,2) sum(Xmax); sum(Xmax) sum(Xmax.^2)];
 MB = [sum(Ylineal);sum(Ylineal.*Xmax)];
 
-Sol = MA\MB;
+Sol = MA\MB; %Esto es equivalente a A inversa por B
 
-A_reg = exp(Sol(1));
+A_reg = exp(Sol(1)); %Obtención de constantes de laa regresion
 k_reg = Sol(2);
 
-y_reg = A_reg*exp(k_reg*x).*(-cos(2*pi*f_reg*x));
+y_reg = A_reg*exp(k_reg*x).*(-cos(2*pi*f_reg*x)); %funcion obtenida de la regresión
 
 %% RÂ²
-
-Sr = sum((y-y_reg).^2);
+%Calculo de coeficiente de determinacion
+Sr = sum((y-y_reg).^2); 
 
 St = var(y)*size(y,2);
 
 R_2 = 1-Sr/St;
 
 %% Graficando original y regresion
-figure(3)
+figure(2)
 plot(x,y,'-o');
 hold on
 plot(x,y_reg,'-x');
+hold off
 %% Calculos de componentes en circuito Salem-Key
 u = 10^-6;
-n = 10^-9
-C1 = 10*u;
+n = 10^-9;
+C1 = 10*u; %Se propone valores iniciales para capacitores
 C2 = 24*n;
 Z = k_reg/sqrt(k_reg^2+(2*pi*f_reg)^2);
 Wn = -k_reg/Z;
-syms R1s R2s;
+syms R1s R2s; %Se despeja para R1 y R2
 S = solve([(R1s+R2s)/(R1s*R2s*C1)==2*Z*Wn, Wn^2==1/(R1s*R2s*C1*C2)],[R1s, R2s])
-close all
-G = tf([Wn^2],[1 2*Wn*Z Wn^2])
 
-step(G)
-R1 = double(S.R1s(2));
-R2 = double(S.R1s(1));
+G = tf([Wn^2],[1 2*Wn*Z Wn^2])
+figure(3)
+step(5*G) %Se grafica la respuesta al escalón de 5V de la función de transferencia
+R1 = double(S.R1s(2)); %Convertir datos de R1 y R2 a formato de numero
+R2 = double(S.R2s(2));
 
 %% Funciones
 
